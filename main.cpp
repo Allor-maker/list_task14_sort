ab5d157192b9d27e5ee91233f889fda9e43e6ee4
P_Alex_Tr <aleksandr.smyshlaev@yandex.ru>
2024-11-11 21:57:56 +0300

    добавил тесты на разное количество элементов

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "list.h"
 #include <vector>
-#include <time.h>
 int main()
 {
 	List l(10);
@@ -22,21 +21,34 @@ int main()
 		l2[i] = a[i];
 	}
 
-	clock_t t_start = clock();
 	l2.sort_list();
-	clock_t t_end = clock();
-	std::cout << float(t_end - t_start) / CLOCKS_PER_SEC << std::endl;
 	l2.print();
 
-	List l3(10000);
+	List l6(15000);
+	for (int i = 0; i < 15000; i++)
+	{
+		l6[i] = rand() % 100;
+	}
+	l6.sort_list();
 
+	List l3(10000);
 	for (int i = 0; i <10000; i++)
 	{
 		l3[i] = rand() % 100;
 	}
-
-	clock_t t2_start = clock();
 	l3.sort_list();
-	clock_t t2_end = clock();
-	std::cout << float(t2_end - t2_start) / CLOCKS_PER_SEC << std::endl;
+
+	List l4(5000);
+	for (int i = 0; i < 5000; i++)
+	{
+		l4[i] = rand() % 100;
+	}
+	l4.sort_list();
+
+	List l5(1000);
+	for (int i = 0; i < 1000; i++)
+	{
+		l5[i] = rand() % 100;
+	}
+	l5.sort_list();
 }
